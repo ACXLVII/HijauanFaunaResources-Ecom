2ac8a5c1e0e163fa2590fb7239c0051fb1c2e1be
EZAD DANIAL <danializani@gmail.com>
2025-10-16 22:43:51 +0800

    Test

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function HomePage() {
         <Welcome />
         <Prices />
         <Services />
-        <Results />
+        <Results /> 
         {/* <Contact /> */}
       </main>
 
